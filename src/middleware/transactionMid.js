7ebd127ef9b867ed7c6d4f9aeb2cfa9bc0985ceb
Muhammad Daffa Raihan <d.raihan2004@gmail.com>
2024-05-04 14:43:34 +0700

    tester mutex for race condition

--- src/middleware/transactionMid.js
+++ src/middleware/transactionMid.js
@@ -1,23 +1,34 @@
 const { AddBuyerDB } = require("../model/buyer");
 const { GetSpesificEventById } = require("../model/event");
 
-const CheckEvent = async(req,res,next)=>{
+const { Mutex } = require('async-mutex');
+const mutex = new Mutex();
+
+const CheckEvent = async (req, res, next) => {
     try {
-        const {id_event} = req.body
+        // Acquire the mutex lock
+        const release = await mutex.acquire();
 
+        const { id_event } = req.body;
         const data = await GetSpesificEventById(id_event);
-        if (data.length > 0) {
-            req.data_event = data[0]
-            next()
-          } else {
-            res.status(404).send({ msg: "data tidak ditemukan" });
-          }
-    } catch (error) {
-        res.status(500).send({ msg: "internal server error" });
 
-        
+        if (data.length > 0) {
+            req.data_event = data[0];
+            // Release the mutex lock before calling next()
+            release();
+            next();
+        } else {
+            // Release the mutex lock before sending the response
+            release();
+            res.status(404).send({ msg: "Data tidak ditemukan" });
+        }
+    } catch (error) {
+        // Release the mutex lock before sending the response
+        release();
+        console.error("Error in CheckEvent middleware:", error);
+        res.status(500).send({ msg: "Internal Server Error" });
     }
-}
+};
 
 const Add_Buyer = async(req, res,next) => {
 
