ac5d946bd2d73761fe04f862fce8e4c4091fbaa8
Muhammad Daffa Raihan <d.raihan2004@gmail.com>
2024-05-09 10:09:35 +0700

    fixing the part of email

--- src/function/mailjet.js
+++ src/function/mailjet.js
@@ -8,38 +8,71 @@ const mailjet = Mailjet.apiConnect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_
 async function sendEmail(data,category) {
   return new Promise(async (resolve, reject) => {
     let pdfBuffer = null
+    let filename = null
+    let request = null
 
     if (category ==="event") {    
        pdfBuffer = await generateTicket(data);
+       filename = "ticket.pdf";
+      request = mailjet.post("send", { version: "v3.1" }).request({
+         Messages: [
+           {
+             From: {
+               Email: process.env.EMAIL_ADDRESS,
+               Name: "TEDXUII",
+             },
+             To: [
+               {
+                 Email: data.buyer_details[0].email,
+                 Name: data.buyer_details[0].username,
+               },
+             ],
+             Subject: "Your Ticket for TEDXUII Event",
+             TextPart: "Greetings from TEDXUII!",
+             HTMLPart: `<h3>Dear ${data.buyer_details[0].username},</h3><p>Please find attached your ticket for the TEDXUII event.</p>`,
+             Attachments: [
+               {
+                 ContentType: "application/pdf",
+                 Filename: filename,
+                 Base64Content: pdfBuffer.toString("base64"),
+               },
+             ],
+           },
+         ],
+       });
     }else if (category ==="merchandise") {
       pdfBuffer = await generateTransactionReceipt(data); 
-    }
-    const request = mailjet.post("send", { version: "v3.1" }).request({
-      Messages: [
-        {
-          From: {
-            Email: process.env.EMAIL_ADDRESS,
-            Name: "TEDXUII",
+      filename = "recipient_merchandise.pdf";
+      request = mailjet.post("send", { version: "v3.1" }).request({
+        Messages: [
+          {
+            From: {
+              Email: process.env.EMAIL_ADDRESS,
+              Name: "TEDXUII",
+            },
+            To: [
+              {
+                Email: data.buyer_details[0].email,
+                Name: data.buyer_details[0].username,
+              },
+            ],
+            Subject: "Your Recipient for TEDXUII Merchandise",
+            TextPart: "Greetings from TEDXUII!",
+            HTMLPart: `<h3>Dear ${data.buyer_details[0].username},</h3>
+            <p>Thank you for your purchase!</p>
+            <p>Please find attached your Recipient for the TEDXUII Merchandise.</p>
+            <p>Thank you for being a valued supporter of TEDXUII!</p>`,
+            Attachments: [
+              {
+                ContentType: "application/pdf",
+                Filename: filename,
+                Base64Content: pdfBuffer.toString("base64"),
+              },
+            ],
           },
-          To: [
-            {
-              Email: data.buyer_details[0].email,
-              Name: data.buyer_details[0].username,
-            },
-          ],
-          Subject: "Your Ticket for TEDXUII Event",
-          TextPart: "Greetings from TEDXUII!",
-          HTMLPart: `<h3>Dear ${data.buyer_details[0].username},</h3><p>Please find attached your ticket for the TEDXUII event.</p>`,
-          Attachments: [
-            {
-              ContentType: "application/pdf",
-              Filename: "ticket.pdf",
-              Base64Content: pdfBuffer.toString("base64"),
-            },
-          ],
-        },
-      ],
-    });
+        ],
+      });
+    }
 
     request
       .then((result) => {
